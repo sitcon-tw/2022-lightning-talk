af8aac68c9599e72eea2757fe380d62d8d36e1e7
Allen Guan <me@nella17.tw>
2022-08-22 12:07:52 +0800

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@pinia/nuxt"],
+  app: {
+    baseURL: '/2022-lightning-talk/'
+  }
 });
