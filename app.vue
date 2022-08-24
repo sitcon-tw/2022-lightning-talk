6ee7f1d26beb4ae9ccaf59ef0d18bc8435805d70
Allen Guan <me@nella17.tw>
2022-08-24 15:22:59 +0800

    fix link in body :(

--- app.vue
+++ app.vue
@@ -14,17 +14,6 @@ onBeforeUnmount(() => {
 
 <template>
   <div id="app">
-    <Head>
-      <link rel="icon" type="image/svg" href="/2022-lightning-talk/favicon.svg" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/2022-lightning-talk/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/2022-lightning-talk/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/2022-lightning-talk/favicon-16x16.png">
-      <link rel="manifest" href="/2022-lightning-talk/site.webmanifest">
-      <!-- Google Fonts -->
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Barlow:wght@500&family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Syncopate:wght@700&family=Roboto+Condensed:wght@400;700&display=swap" crossorigin="true" />
-    </Head>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,46 @@ export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
   app: {
     baseURL: '/2022-lightning-talk/',
+    head: {
+      link: [
+        // Favicon
+        {
+          rel: 'icon',
+          type: 'image/svg',
+          href: '/2022-lightning-talk/favicon.svg',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/2022-lightning-talk/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/2022-lightning-talk/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/2022-lightning-talk/favicon-16x16.png',
+        },
+        { rel: 'manifest', href: '/2022-lightning-talk/site.webmanifest' },
+        // Google Fonts
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'true',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Barlow:wght@500&family=Noto+Sans+TC:wght@100;300;400',
+          crossorigin: 'true',
+        },
+      ],
+    },
   },
   nitro: {
     prerender: {
