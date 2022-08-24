970d5343c813ae4d106166f9307a22825ce65b67
Allen Guan <me@nella17.tw>
2022-08-24 08:23:00 +0800

    prerender 404.html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt"],
+  modules: ['@pinia/nuxt'],
   app: {
-    baseURL: '/2022-lightning-talk/'
-  }
-});
+    baseURL: '/2022-lightning-talk/',
+  },
+  nitro: {
+    prerender: {
+      routes: ['/404.html'],
+    },
+  },
+})
