e32419a34f1e5ca5ea7d79812add2332ff0f5c0a
Allen Guan <me@nella17.tw>
2022-08-29 03:34:10 +0800

    implement post

--- pages/post.vue
+++ pages/post.vue
@@ -3,20 +3,32 @@ import { storeToRefs } from 'pinia'
 
 const store = useStore()
 store.title = '投稿頁面'
+const { token } = storeToRefs(store)
 
-const name = ref('')
-const title = ref('')
-const description = ref('')
-const contact = ref('')
+const form = ref(null)
 
-const submit = () => {
-  const data = {
-    name: name.value,
-    title: title.value,
-    description: description.value,
-    contact: contact.value,
-  }
-  console.log(data)
+const data = ref({
+  name: '',
+  title: '',
+  description: '',
+  contact: '',
+})
+
+const submit = async () => {
+  if (!form.value.reportValidity())
+    return false
+  const res = await fetch(`${config.worker_url}/post`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: unref(token),
+      ...unref(data),
+    }),
+  }).then(res => res.json())
+  if (res.uuid) store.status.post = true
+  if (res.message) alert(res.message)
 }
 </script>
 
@@ -25,25 +37,26 @@ const submit = () => {
     <non-token-modal  />
 
     <div class="page">
-      <span class="text">講者姓名</span>
-      <input type="text" v-model="name" placeholder="請輸入姓名..." />
+      <form class="inputs" ref="form" @submit.prevent="submit">
+        <span class="text">講者姓名</span>
+        <input type="text" v-model="data.name" placeholder="請輸入姓名..." required />
 
-      <span class="text">議題</span>
-      <input type="text" v-model="title" placeholder="請輸入議題..." />
+        <span class="text">議題</span>
+        <input type="text" v-model="data.title" placeholder="請輸入議題..." required />
 
-      <span class="text">摘要</span>
-      <textarea v-model="description" placeholder="請輸入摘要..."></textarea>
+        <span class="text">摘要</span>
+        <textarea v-model="data.description" placeholder="請輸入摘要..." required></textarea>
 
-      <span class="text">聯絡方式</span>
-      <input type="text" v-model="contact" placeholder="請輸入聯絡方式..." />
-
+        <span class="text">聯絡方式</span>
+        <input type="text" v-model="data.contact" placeholder="請輸入聯絡方式..." required />
+      </form>
       <btn class="submit" @click="submit">送出</btn>
     </div>
   </div>
 </template>
 
 <style scoped lang="sass">
-.form
+.inputs
   height: 100%
   display: flex
   flex-direction: column
@@ -63,7 +76,10 @@ input, textarea
   background: #EBE1CC
   &:focus
     outline: 1px solid #82D357
+  &:required:invalid
+    outline: 1px solid #FF5252
 textarea
   flex: 1
   padding-top: 16px
+  resize: none
 </style>
