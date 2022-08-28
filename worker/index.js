194b94ece1e37a730e5198463fb347ff01cfd744
Allen Guan <me@nella17.tw>
2022-08-29 03:15:59 +0800

    fix worker

--- worker/index.js
+++ worker/index.js
@@ -12,9 +12,22 @@ function badRequest(message = 'Bad Request', status = 400) {
   return responseJson({ message }, { status })
 }
 
+function add_cors_headers(req, res) {
+  res.headers.set('Access-Control-Allow-Origin', req.headers.get('Origin'))
+  res.headers.set('Access-Control-Allow-Headers', req.headers.get('Access-Control-Request-Headers'))
+  res.headers.set('Access-Control-Allow-Methods', req.headers.get('Access-Control-Request-Method'))
+  return res
+}
+
+function check_action(action) {
+  if (!action) return badRequest('action is required')
+  if (!config.actions.some(v => v === action)) return badRequest('action is invalid')
+  return true
+}
+
 function requestData(action, body = null) {
-  if (!config.actions.some(v => v === action))
-    return badRequest(`Action '${action}' is not allowed.`)
+  const res = check_action(action)
+  if (res !== true) return res
   const opt = {}
   if (body) {
     opt.method = 'POST'
@@ -22,7 +35,8 @@ function requestData(action, body = null) {
     if (!opt.headers) opt.headers = {}
     opt.headers['Content-Type'] = 'application/json'
   }
-  return fetch(`${config.server_url}?action=${encodeURIComponent(action)}`, opt).then(res => res.json())
+  return fetch(`${config.server_url}?action=${encodeURIComponent(action)}`, opt)
+    .then(res => res.json())
 }
 
 router.get('/:action', async (request) => {
@@ -42,8 +56,21 @@ router.post('/:action', async (request) => {
   return responseJson(returnData)
 })
 
+router.options('/:action', async (request) => {
+  const action = request.params.action
+  const res = check_action(action)
+  if (res !== true) return res
+  return responseJson({})
+})
+
 router.all('*', () => badRequest('404, not found!', 404))
 
 addEventListener('fetch', (e) => {
-  e.respondWith(router.handle(e.request))
+  const resp = router.handle(e.request)
+    .then(res => add_cors_headers(e.request, res))
+    .catch(e => {
+      console.error(e)
+      return responseJson({ message: 'Internal Server Error' }, 500)
+    })
+  e.respondWith(resp)
 })
