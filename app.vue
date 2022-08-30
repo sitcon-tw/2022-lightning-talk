b12cc79c347ce32e50e828b30369f9a518d7bf40
Allen Guan <me@nella17.tw>
2022-08-30 17:29:50 +0800

    add gtm

--- app.vue
+++ app.vue
@@ -18,6 +18,18 @@ if (process.client) useEventListener(window, 'resize', updateVh)
 onMounted(() => {
   if (process.client) updateVh()
 })
+
+if (process.client) {
+  ! function (e, t, a, n, g) {
+    e[n] = e[n] || [], e[n].push({
+      "gtm.start": (new Date).getTime(),
+      event: "gtm.js"
+    });
+    var m = t.getElementsByTagName(a)[0],
+      r = t.createElement(a);
+    r.async = !0, r.src = "https://www.googletagmanager.com/gtm.js?id=GTM-NPVBCDZ", m.parentNode.insertBefore(r, m)
+  }(window, document, "script", "dataLayer");
+}
 </script>
 
 <template>
