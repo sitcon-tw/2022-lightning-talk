32d6d49204c43731d98d65f0edb6628d35617b75
Allen Guan <me@nella17.tw>
2022-08-31 08:25:02 +0800

    set default seed

--- composables/shuffle.js
+++ composables/shuffle.js
@@ -2,7 +2,7 @@ import seedrandom from 'seedrandom'
 
 export const shuffle = (array) => {
 	const { token } = useStore()
-	const rng = seedrandom(token)
+	const rng = seedrandom(token ?? 'default_s33d')
 	const size = array.length
 	const idxs = Array.from({ length: size }, (_, idx) => ({ idx, rng: rng() }))
 	idxs.sort((a, b) => a.rng - b.rng)
