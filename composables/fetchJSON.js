6847f671ddfe08dd469af22b98826b92f57b7ef4
gnehs <jayuiop@gmail.com>
2022-09-04 09:36:54 +0800

    updated fetchJSON

--- composables/fetchJSON.js
+++ composables/fetchJSON.js
@@ -2,15 +2,20 @@ export const fetchJSON = async (...args) => {
   if (!process.client) return {}
   const store = useStore()
   store.loading += 1
-  try {
-    const res = await window.fetch(...args).then((res) => res.json())
-    return res
-  } catch (err) {
-    console.error(err)
-    return {
-      message: `Error: ${err.message}\nContact the developer if this problem persists.\n\nDebug Info:\n${err.stack}`,
-    }
-  } finally {
+
+  let res = await window.fetch(...args).then(async response => {
     store.loading -= 1
-  }
+    if (response.ok) {
+      return (await res.json())
+    } else if (response.status == 403) {
+      return {
+        message: `請使用會場提供的 Wi-Fi`,
+      }
+    } else {
+      console.error(response)
+      return {
+        message: `發生錯誤，請稍後再試`,
+      }
+    }
+  })
 }
